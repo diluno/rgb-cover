af7912d9106c5e215aa28657350498f781c21d18
Samuel Alder <samuel.alder@gmail.com>
2024-11-16 17:12:49 +0100

    Fix path

--- index.js
+++ index.js
@@ -53,12 +53,12 @@ function checkCover(_entities) {
       file.on('finish', () => {
         file.close();
 
-        const img = '/home/sam/hyper-dash/idotmatrix/cover.jpg';
+        const img = '/home/sam/rgb-cover/cover.jpg';
         if (child) {
           child.kill('SIGKILL');
         }
         child = exec(
-          '/home/sam/rpi-rgb-led-matrix/utils/led-image-viewer --led-rows=64 --led-cols=64 --led-gpio-mapping=adafruit-hat-pwm --led-brightness=90 --led-pwm-lsb-nanoseconds=80 --led-limit-refresh=100 /home/sam/hyper-dash/idotmatrix/cover.jpg',
+          '/home/sam/rpi-rgb-led-matrix/utils/led-image-viewer --led-rows=64 --led-cols=64 --led-gpio-mapping=adafruit-hat-pwm --led-brightness=90 --led-pwm-lsb-nanoseconds=80 --led-limit-refresh=100 /home/sam/rgb-cover/cover.jpg',
           { shell: '/bin/bash', detached: true }
         );
         child.on('error', (err) => {
